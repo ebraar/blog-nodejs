31c49c1ece25abab6349f7e85f3b55e9516796a3
ebraar <ebbet468@gmail.com>
2024-07-09 16:12:42 +0300

    update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,12 +2,9 @@ const user = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const Response = require("../utils/response");
 const APIError = require("../utils/errors");
-const { createToken, createTemporaryToken, decodedTemporaryToken } = require("../middlewares/auth");
-const crypto = require("crypto");
-const moment = require("moment");
-const sendEmail = require("../utils/sendMail");
+const { createToken } = require("../middlewares/auth");
 
-
+// giriş işlemi
 const login = async (req, res) => {
     console.log("login");
     const { email, password } = req.body;
@@ -23,7 +20,7 @@ const login = async (req, res) => {
   
     createToken(userInfo, res);
   };
-  
+  // kayıt işlemi
   const register = async (req, res) => {
     const { email } = req.body;
   
@@ -49,103 +46,8 @@ const login = async (req, res) => {
       });
   };
   
-  
-  const forgetPassword = async (req, res) => {
-    const { email } = req.body;
-  
-    const userInfo = await user
-      .findOne({ email })
-      .select(" name lastname email ");
-  
-    if (!userInfo) return new APIError("Geçersiz Kullanıcı", 400);
-  
-    console.log("userInfo : ", userInfo);
-  
-    const resetCode = crypto.randomBytes(3).toString("hex");
-  
-    console.log(resetCode);
-
-    // await sendEmail({
-    //     from: "base.api.proje@outlook.com",
-    //     to: userInfo.email,
-    //     subject: "Şifre Sıfırlama",
-    //     text: `Şifre Sıfırlama Kodunuz ${resetCode}`
-    // })
-  
-    await user.updateOne(
-      { email },
-      {
-        reset: {
-          code: resetCode,
-          time: moment(new Date())
-            .add(15, "minute")
-            .format("YYYY-MM-DD HH:mm:ss"),
-        },
-      }
-    );
-  
-    return new Response(true, "Lütfen Mail Kutunuzu Kontrol Ediniz").success(res);
-  };
-  
-  const resetCodeCheck = async (req, res) => {
-    const { email, code } = req.body;
-  
-    const userInfo = await user
-      .findOne({ email })
-      .select("_id name lastname email reset");
-  
-    if (!userInfo) throw new APIError("Geçersiz Kod !", 401);
-  
-    const dbTime = moment(userInfo.reset.time);
-    const nowTime = moment(new Date());
-  
-    const timeDiff = dbTime.diff(nowTime, "minutes");
-  
-    console.log("Zaman farkı : ", timeDiff);
-  
-    if (timeDiff <= 0 || userInfo.reset.code !== code) {
-      throw new APIError("Geçersiz Kod", 401);
-    }
-  
-    const temporaryToken = await createTemporaryToken(
-      userInfo._id,
-      userInfo.email
-    );
-  
-    return new Response(
-      { temporaryToken },
-      "Şifre Sıfırlama Yapabilirsiniz"
-    ).success(res);
-  };
-  
-  const resetPassword = async (req, res) => {
-    const { password, temporaryToken } = req.body;
-  
-    const decodedToken = await decodedTemporaryToken(temporaryToken);
-    console.log("decodedToken : ", decodedToken);
-  
-    const hashPassword = await bcrypt.hash(password, 10);
-  
-    await user.findByIdAndUpdate(
-      { _id: decodedToken._id },
-      {
-        reset: {
-          code: null,
-          time: null,
-        },
-        password: hashPassword,
-      }
-    );
-  
-    return new Response(decodedToken, "Şifre Sıfırlama Başarılı").success(res)
-  };
-
-  
 module.exports = {
     login,
-    register,
-    forgetPassword,
-    resetCodeCheck,
-    resetPassword,
+    register
   };
   
